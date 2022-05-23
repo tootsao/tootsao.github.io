bce916c9f59f1ea58c692e17b049bcf4d5a11309
tootsao <67867120+tootsao@users.noreply.github.com>
2022-05-23 18:26:22 +0200

    Update countdown_v2.js

--- countdown_v2.js
+++ countdown_v2.js
@@ -1,5 +1,8 @@
 let startingDate = new Date('May 13, 2022 15:53').getTime();
-let counter = document.querySelector('.countup-timer');
+let counter1 = document.querySelector('.countup-timer1');
+let counter2 = document.querySelector('.countup-timer2');
+let counter3 = document.querySelector('.countup-timer3');
+let counter4 = document.querySelector('.countup-timer4');
 
 setInterval(function() {
     let newDate = new Date().getTime();
@@ -10,5 +13,8 @@ setInterval(function() {
     let minutes = Math.floor((finalDate % (1000 * 60 *60)) / (1000 * 60));
     let seconds = Math.floor((finalDate % (1000 * 60)) / 1000);
 
-    counter.innerHTML = days+" days\n"+hours+" hours "+minutes+" minutes "+seconds+" seconds";
+    counter1.innerHTML = days+" days";
+    counter2.innerHTML = hours+" hours";
+    counter3.innerHTML = minutes+" minutes";
+    counter4.innerHTML = seconds+" seconds";
 }, 1000) 
